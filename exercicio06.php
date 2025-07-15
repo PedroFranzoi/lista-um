da262e33755c0db39112e1547c78a270bd998f8a
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-07-15 10:48:11 -0300

    Questão 6

---
+++ exercicio06.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exibir todos os divisores de um número</title>
+</head>
+
+<body>
+    <form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['verificar'])) {
+            $numero = $_POST['numero'];
+
+            for($i = 1; $i <= $numero; $i++){
+                if($numero %$i == 0){
+                    echo $i;
+                    echo "<br>";
+                };
+            };
+
+        };
+    };
+
+    ?>
+
+</body>
+
+</html>
